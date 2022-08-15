1e6df2a0d3536f218ab437e05adcd89ac142ded4
Ilkay Mehmet Bora <73322500+ilkaymb@users.noreply.github.com>
2022-08-15 22:41:29 +0300

    Add files via upload

---
+++ XOX_oyun/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿
+namespace XOX_oyun
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pos_zero_button = new System.Windows.Forms.Button();
+            this.pos_one_button = new System.Windows.Forms.Button();
+            this.pos_two_button = new System.Windows.Forms.Button();
+            this.pos_seven_button = new System.Windows.Forms.Button();
+            this.pos_six_button = new System.Windows.Forms.Button();
+            this.pos_eight_button = new System.Windows.Forms.Button();
+            this.pos_four_button = new System.Windows.Forms.Button();
+            this.pos_five_button = new System.Windows.Forms.Button();
+            this.pos_tree_button = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.XOX = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pos_zero_button
+            // 
+            this.pos_zero_button.BackColor = System.Drawing.Color.Brown;
+            this.pos_zero_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pos_zero_button.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.pos_zero_button.FlatAppearance.BorderSize = 3;
+            this.pos_zero_button.Font = new System.Drawing.Font("Yu Gothic", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.pos_zero_button.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pos_zero_button.Location = new System.Drawing.Point(18, 86);
+            this.pos_zero_button.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.pos_zero_button.Name = "pos_zero_button";
+            this.pos_zero_button.Size = new System.Drawing.Size(88, 75);
+            this.pos_zero_button.TabIndex = 0;
+            this.pos_zero_button.TabStop = false;
+            this.pos_zero_button.UseVisualStyleBackColor = false;
+            this.pos_zero_button.Click += new System.EventHandler(this.pos_zero_button_Click);
+            // 
+            // pos_one_button
+            // 
+            this.pos_one_button.BackColor = System.Drawing.Color.Brown;
+            this.pos_one_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pos_one_button.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.pos_one_button.FlatAppearance.BorderSize = 3;
+            this.pos_one_button.Font = new System.Drawing.Font("Yu Gothic", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.pos_one_button.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pos_one_button.Location = new System.Drawing.Point(122, 86);
+            this.pos_one_button.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.pos_one_button.Name = "pos_one_button";
+            this.pos_one_button.Size = new System.Drawing.Size(88, 75);
+            this.pos_one_button.TabIndex = 0;
+            this.pos_one_button.TabStop = false;
+            this.pos_one_button.UseVisualStyleBackColor = false;
+            this.pos_one_button.Click += new System.EventHandler(this.pos_one_button_Click);
+            // 
+            // pos_two_button
+            // 
+            this.pos_two_button.BackColor = System.Drawing.Color.Brown;
+            this.pos_two_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pos_two_button.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.pos_two_button.FlatAppearance.BorderSize = 3;
+            this.pos_two_button.Font = new System.Drawing.Font("Yu Gothic", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.pos_two_button.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pos_two_button.Location = new System.Drawing.Point(228, 86);
+            this.pos_two_button.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.pos_two_button.Name = "pos_two_button";
+            this.pos_two_button.Size = new System.Drawing.Size(88, 75);
+            this.pos_two_button.TabIndex = 0;
+            this.pos_two_button.TabStop = false;
+            this.pos_two_button.UseVisualStyleBackColor = false;
+            this.pos_two_button.Click += new System.EventHandler(this.pos_two_button_Click);
+            // 
+            // pos_seven_button
+            // 
+            this.pos_seven_button.BackColor = System.Drawing.Color.Brown;
+            this.pos_seven_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pos_seven_button.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.pos_seven_button.FlatAppearance.BorderSize = 3;
+            this.pos_seven_button.Font = new System.Drawing.Font("Yu Gothic", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.pos_seven_button.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pos_seven_button.Location = new System.Drawing.Point(123, 281);
+            this.pos_seven_button.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.pos_seven_button.Name = "pos_seven_button";
+            this.pos_seven_button.Size = new System.Drawing.Size(88, 75);
+            this.pos_seven_button.TabIndex = 0;
+            this.pos_seven_button.TabStop = false;
+            this.pos_seven_button.UseVisualStyleBackColor = false;
+            this.pos_seven_button.Click += new System.EventHandler(this.pos_seven_button_Click);
+            // 
+            // pos_six_button
+            // 
+            this.pos_six_button.BackColor = System.Drawing.Color.Brown;
+            this.pos_six_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pos_six_button.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.pos_six_button.FlatAppearance.BorderSize = 3;
+            this.pos_six_button.Font = new System.Drawing.Font("Yu Gothic", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.pos_six_button.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pos_six_button.Location = new System.Drawing.Point(18, 281);
+            this.pos_six_button.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.pos_six_button.Name = "pos_six_button";
+            this.pos_six_button.Size = new System.Drawing.Size(88, 75);
+            this.pos_six_button.TabIndex = 0;
+            this.pos_six_button.TabStop = false;
+            this.pos_six_button.UseVisualStyleBackColor = false;
+            this.pos_six_button.Click += new System.EventHandler(this.pos_six_button_Click);
+            // 
+            // pos_eight_button
+            // 
+            this.pos_eight_button.BackColor = System.Drawing.Color.Brown;
+            this.pos_eight_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pos_eight_button.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.pos_eight_button.FlatAppearance.BorderSize = 3;
+            this.pos_eight_button.Font = new System.Drawing.Font("Yu Gothic", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.pos_eight_button.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pos_eight_button.Location = new System.Drawing.Point(228, 281);
+            this.pos_eight_button.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.pos_eight_button.Name = "pos_eight_button";
+            this.pos_eight_button.Size = new System.Drawing.Size(88, 75);
+            this.pos_eight_button.TabIndex = 0;
+            this.pos_eight_button.TabStop = false;
+            this.pos_eight_button.UseVisualStyleBackColor = false;
+            this.pos_eight_button.Click += new System.EventHandler(this.pos_eight_button_Click);
+            // 
+            // pos_four_button
+            // 
+            this.pos_four_button.BackColor = System.Drawing.Color.Brown;
+            this.pos_four_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pos_four_button.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.pos_four_button.FlatAppearance.BorderSize = 3;
+            this.pos_four_button.Font = new System.Drawing.Font("Yu Gothic", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.pos_four_button.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pos_four_button.Location = new System.Drawing.Point(123, 183);
+            this.pos_four_button.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.pos_four_button.Name = "pos_four_button";
+            this.pos_four_button.Size = new System.Drawing.Size(88, 75);
+            this.pos_four_button.TabIndex = 0;
+            this.pos_four_button.TabStop = false;
+            this.pos_four_button.UseVisualStyleBackColor = false;
+            this.pos_four_button.Click += new System.EventHandler(this.pos_four_button_Click);
+            // 
+            // pos_five_button
+            // 
+            this.pos_five_button.BackColor = System.Drawing.Color.Brown;
+            this.pos_five_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pos_five_button.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.pos_five_button.FlatAppearance.BorderSize = 3;
+            this.pos_five_button.Font = new System.Drawing.Font("Yu Gothic", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.pos_five_button.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pos_five_button.Location = new System.Drawing.Point(228, 183);
+            this.pos_five_button.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.pos_five_button.Name = "pos_five_button";
+            this.pos_five_button.Size = new System.Drawing.Size(88, 75);
+            this.pos_five_button.TabIndex = 0;
+            this.pos_five_button.TabStop = false;
+            this.pos_five_button.UseVisualStyleBackColor = false;
+            this.pos_five_button.Click += new System.EventHandler(this.pos_five_button_Click);
+            // 
+            // pos_tree_button
+            // 
+            this.pos_tree_button.BackColor = System.Drawing.Color.Brown;
+            this.pos_tree_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pos_tree_button.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.pos_tree_button.FlatAppearance.BorderSize = 3;
+            this.pos_tree_button.Font = new System.Drawing.Font("Yu Gothic", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.pos_tree_button.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pos_tree_button.Location = new System.Drawing.Point(18, 183);
+            this.pos_tree_button.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.pos_tree_button.Name = "pos_tree_button";
+            this.pos_tree_button.Size = new System.Drawing.Size(88, 75);
+            this.pos_tree_button.TabIndex = 0;
+            this.pos_tree_button.TabStop = false;
+            this.pos_tree_button.UseVisualStyleBackColor = false;
+            this.pos_tree_button.Click += new System.EventHandler(this.pos_tree_button_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Brown;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.XOX);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(364, 75);
+            this.panel1.TabIndex = 1;
+            // 
+            // XOX
+            // 
+            this.XOX.AutoSize = true;
+            this.XOX.Font = new System.Drawing.Font("Yu Gothic UI", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.XOX.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.XOX.Location = new System.Drawing.Point(108, 0);
+            this.XOX.Name = "XOX";
+            this.XOX.Size = new System.Drawing.Size(134, 71);
+            this.XOX.TabIndex = 0;
+            this.XOX.Text = "XOX";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Linen;
+            this.ClientSize = new System.Drawing.Size(347, 381);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pos_two_button);
+            this.Controls.Add(this.pos_tree_button);
+            this.Controls.Add(this.pos_six_button);
+            this.Controls.Add(this.pos_five_button);
+            this.Controls.Add(this.pos_four_button);
+            this.Controls.Add(this.pos_eight_button);
+            this.Controls.Add(this.pos_seven_button);
+            this.Controls.Add(this.pos_one_button);
+            this.Controls.Add(this.pos_zero_button);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button pos_zero_button;
+        private System.Windows.Forms.Button pos_one_button;
+        private System.Windows.Forms.Button pos_two_button;
+        private System.Windows.Forms.Button pos_seven_button;
+        private System.Windows.Forms.Button pos_six_button;
+        private System.Windows.Forms.Button pos_eight_button;
+        private System.Windows.Forms.Button pos_four_button;
+        private System.Windows.Forms.Button pos_five_button;
+        private System.Windows.Forms.Button pos_tree_button;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label XOX;
+    }
+}
+
---
+++ XOX_oyun/Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace XOX_oyun
+{
+
+    public partial class Form1 : Form
+    {
+        public bool turn_checker = true;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void pos_zero_button_Click(object sender, EventArgs e)
+        {
+            if (turn_checker)
+                pos_zero_button.Text = "X";
+            else
+                pos_zero_button.Text = "O";
+
+
+            pos_zero_button.Enabled = false;
+
+            control_unite();
+
+        }
+
+        private void pos_one_button_Click(object sender, EventArgs e)
+        {
+            if (turn_checker)
+                pos_one_button.Text = "X";
+            else
+                pos_one_button.Text = "O";
+
+
+            pos_one_button.Enabled = false;
+            control_unite();
+
+        }
+
+        private void pos_two_button_Click(object sender, EventArgs e)
+        {
+            if (turn_checker)
+                pos_two_button.Text = "X";
+            else
+                pos_two_button.Text = "O";
+
+
+            pos_two_button.Enabled = false;
+            control_unite();
+        }
+
+        private void pos_tree_button_Click(object sender, EventArgs e)
+        {
+            if (turn_checker)
+                pos_tree_button.Text = "X";
+            else
+                pos_tree_button.Text = "O";
+
+
+            pos_tree_button.Enabled = false;
+            control_unite();
+
+        }
+
+        private void pos_four_button_Click(object sender, EventArgs e)
+        {
+            if (turn_checker)
+                pos_four_button.Text = "X";
+            else
+                pos_four_button.Text = "O";
+
+
+            pos_four_button.Enabled = false;
+            control_unite();
+
+        }
+
+        private void pos_five_button_Click(object sender, EventArgs e)
+        {
+            if (turn_checker)
+                pos_five_button.Text = "X";
+            else
+                pos_five_button.Text = "O";
+
+
+            pos_five_button.Enabled = false;
+            control_unite();
+
+        }
+
+        private void pos_six_button_Click(object sender, EventArgs e)
+        {
+            if (turn_checker)
+                pos_six_button.Text = "X";
+            else
+                pos_six_button.Text = "O";
+
+
+            pos_six_button.Enabled = false;
+            control_unite();
+
+        }
+
+        private void pos_seven_button_Click(object sender, EventArgs e)
+        {
+            if (turn_checker)
+                pos_seven_button.Text = "X";
+            else
+                pos_seven_button.Text = "O";
+
+
+            pos_seven_button.Enabled = false;
+            control_unite();
+
+
+        }
+
+        private void pos_eight_button_Click(object sender, EventArgs e)
+        {
+            if (turn_checker)
+                pos_eight_button.Text = "X";
+            else
+                pos_eight_button.Text = "O";
+
+
+            pos_eight_button.Enabled = false;
+            control_unite();
+
+
+        }
+        public string[,] matrix_replacer()
+        {
+            string[,] matrix = new string[3, 3]{ {pos_zero_button.Text.ToString(),pos_one_button.Text.ToString(),pos_two_button.Text.ToString() },
+                { pos_tree_button.Text.ToString(),pos_four_button.Text.ToString(),pos_five_button.Text.ToString() },
+                { pos_six_button.Text.ToString(),pos_seven_button.Text.ToString(),pos_eight_button.Text.ToString() }
+            };
+            return matrix;
+
+        }
+
+        public void control_unite()
+        {
+            win_checker();
+
+            turn_checker = !turn_checker;
+
+        }
+
+        public int counter = default;
+        public void win_checker()
+        {
+            string[,] matrix = matrix_replacer();
+
+            if (turn_checker)
+            {
+                x_axis_checker("X", matrix);
+
+                y_axis_checker("X", matrix);
+
+                corner_checker("X", matrix);
+
+            }
+            else
+            {
+                x_axis_checker("O", matrix);
+
+                y_axis_checker("O", matrix);
+
+                corner_checker("O", matrix);
+
+            }
+
+        }
+
+        public void x_axis_checker(string symbol, string[,] matrix)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (matrix[i, j] == symbol)
+                    {
+                        counter++;
+                        if (counter == 3)
+                            switch (MessageBox.Show($"TEBRIKLER {symbol} KAZANDI", "kazanma", MessageBoxButtons.RetryCancel, MessageBoxIcon.Information))
+                            {
+                                case DialogResult.Cancel:
+                                    Application.Exit();
+                                    break;
+                                case DialogResult.Retry:
+                                    Form1 form = new Form1();
+                                    form.Show();
+                                    this.Hide();
+                                    break;
+                                
+                            }
+
+                    }
+                    else
+                    {
+                        counter = default;
+                        break;
+                    }
+                }
+            }
+        }
+        public void y_axis_checker(string symbol, string[,] matrix)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (matrix[j, i] == symbol)
+                    {
+                        counter++;
+                        if (counter == 3)
+                            switch (MessageBox.Show($"TEBRIKLER {symbol} KAZANDI", "kazanma", MessageBoxButtons.RetryCancel, MessageBoxIcon.Information))
+                            {
+                                case DialogResult.Cancel:
+                                    Application.Exit();
+                                    break;
+                                case DialogResult.Retry:
+                                    Form1 form = new Form1();
+                                    form.Show();
+                                    this.Hide();
+                                    break;
+
+                            }
+                    }
+                    else
+                    {
+                        counter = default;
+                        break;
+                    }
+                }
+            }
+        }
+
+        public void corner_checker(string symbol, string[,] matrix)
+        {
+            if (matrix[0, 0] == symbol && matrix[1, 1] == symbol && matrix[2, 2] == symbol)
+            {
+                switch (MessageBox.Show($"TEBRIKLER {symbol} KAZANDI", "kazanma", MessageBoxButtons.RetryCancel, MessageBoxIcon.Information))
+                {
+                    case DialogResult.Cancel:
+                        Application.Exit();
+                        break;
+                    case DialogResult.Retry:
+                        Form1 form = new Form1();
+                        form.Show();
+                        this.Hide();
+                        break;
+
+                };
+            }
+            else if (matrix[0, 2] == symbol && matrix[1, 1] == symbol && matrix[2, 0] == symbol)
+            {
+                switch (MessageBox.Show($"TEBRIKLER {symbol} KAZANDI", "kazanma", MessageBoxButtons.RetryCancel, MessageBoxIcon.Information))
+                {
+                    case DialogResult.Cancel:
+                        Application.Exit();
+                        break;
+                    case DialogResult.Retry:
+                        Form1 form = new Form1();
+                        form.Show();
+                        this.Hide();
+                        break;
+
+                }
+
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
